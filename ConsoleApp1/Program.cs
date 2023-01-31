a5439721e78f4517364475fa9d215293f58cb865
Zyglin <zyglin@mail.ru>
2023-02-01 00:48:13 +0300

    Init

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "test";
+            int a = 23;
+            double b = 23.4;
+            bool c = false;
+
+            var value = Console.ReadLine();
+            Console.WriteLine(value);
+            Console.WriteLine($"Вы ввели число {value}");
+            Console.ReadLine();
+        }
+    }
+}
