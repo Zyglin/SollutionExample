640ecec4052be33a31c8e3f2696d6fca350bfd99
Zyglin <zyglin@mail.ru>
2023-03-28 22:04:08 +0300

    lesson 18

---
+++ OnlinePizzaWebShop-master/OnlinePizzaWebApplication/OnlinePizzaWebApplication/ViewModels/SearchPizzasViewModel.cs
@@ -0,0 +1,16 @@
+﻿using OnlinePizzaWebApplication.Models;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlinePizzaWebApplication.ViewModels
+{
+    public class SearchPizzasViewModel
+    {
+        [Required]
+        [DisplayName("Search")]
+        public string SearchText { get; set; }
+        public IEnumerable<Pizzas> PizzaList { get; set; }
+
+    }
+}
