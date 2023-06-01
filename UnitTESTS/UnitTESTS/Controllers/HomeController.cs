977da42229caf91fae4e0bc338bb7679d5279ed3
Zyglin <zyglin@mail.ru>
2023-06-02 00:33:50 +0300

    UnitTests

---
+++ UnitTESTS/UnitTESTS/Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
+using UnitTESTS.Models;
+
+namespace UnitTESTS.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRepository _repository;
+
+        public HomeController(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        //public double Sum(double first, double second )
+        //{
+        //    return first + second;
+        //}
+
+        //public User GetUser(int id, int age, string name)
+        //{
+        //    if (age > 20)
+        //    {
+        //        return new User() { Id = id, Age = age, Name = name };
+        //    }
+        //    else
+        //    {
+        //        return new User() { Id = 0, Age = 0, Name = "Default" };
+        //    }
+        //}
+
+        public IEnumerable<Car> GetCarsInfos()
+        {
+            return _repository.GetCars();
+        }
+    }
+
+    public interface IRepository
+    {
+        IEnumerable<Car> GetCars();
+    }
+
+    public class Car
+    {
+        public int Id { get; set; }
+        public int Year { get; set; }
+        public string Name { get; set; }
+
+        public Car()
+        {
+
+        }
+        public Car(int id, int year, string name)
+        {
+            Id = id;
+            Year = year;
+            Name = name;
+        }
+    }
+}
---
+++ UnitTESTS/WebApplication2Tests/UnitTests/HomeControllerTests.cs
@@ -0,0 +1,133 @@
+﻿using Moq;
+using UnitTESTS.Controllers;
+
+namespace WebApplication2Tests.UnitTests
+{
+    //тело теста формируется по паттерну ААА
+    //Arrange - подготовка данных для тестирования 
+    //Act - то действие которое мы тестируем
+    //Assert - проверка результата  выполнения действия 
+
+
+
+    // использование конструктора это не лучший подход.
+
+
+    //Правила Именования теста: [ТестируемыйМетод_Сценарий_ОжидаемыйРезультат]
+
+
+    // Признаки хорошего юнит теста 
+    // 1) Защита от багов
+    // 2) Устойчиваость к рефакторингу
+    // 3) Быстрая обратная связь
+    // 4) Простота поддержки
+
+    // Существует совместная внепроцесорная зависимость.Иногда чтобы выполнить юнит тест
+    // нам нужно заменить эти зависимости заглушками.Чтобы протестировать поведение без учёта работы зависимостей.
+    // Заглушка - это общий термин описывающий любые разновидности фиктивных зависимостей,
+    // используемых в тестах (Moq)
+    // Moq - инструмент позволяющий использовать заглушки
+
+    public class HomeControllerTests
+    {
+        //private MyClass _data;
+
+        //public HomeControllerTests()
+        //{
+        //    _data = new MyClass(10);
+        //}
+
+        //[Fact]
+        //public void Sum_SumOfTwoNumbers_ReturnsSum()
+        //{
+        //    //Arrange
+        //    double first = 10;
+        //    double second = 20;
+
+        //    var sut = new HomeController();
+
+        //    // Act 
+        //    double result = sut.Sum(first, second);
+
+        //    // Assert
+        //    Assert.Equal(30, result);
+
+        //    // Assert позволяет проверить резултат теста 
+
+        //    // стоит избегать в Юнит тестах команду if, это является антипаттерном.
+        //    // Тест должен быть простым и без ветвлений. В таком случае лучше разбить тест
+        //    // на несколько тестов
+
+        //    // Arrange  самая большая секция 
+        //    // Act обычно состоит из одной строки 
+
+        //    // в тестах так же могут быть конкструкторы 
+        //}
+
+        //[Fact]
+        //public void Test2()
+        //{
+        //    double first = 10;
+        //    double second = 20;
+
+        //    var sut = new HomeController();
+
+
+        //    double result = sut.Sum(first, second);
+
+
+        //    Assert.Equal(30, result);
+        //}
+
+        //class MyClass
+        //{
+        //    private int _val;
+        //    public MyClass(int val)
+        //    {
+        //        _val = val;
+        //    }
+        //}
+
+
+        // тест вызовется 4 раза 
+        //[Theory]
+        //[InlineData(1, 20, "Egor")]
+        //[InlineData(1, 21, "Maks")]
+        //[InlineData(1, 22, "Tom")]
+        //[InlineData(1, 23, "Ivan")]
+        //public void GetUser_ReturnsUserAge20AndMore(int id, int age, string name)
+        //{
+        //    var sut = new HomeController();
+
+        //    var result = sut.GetUser(id, age, name);
+
+        //    Assert.NotEqual("Default", result.Name);
+
+        //}
+
+        [Fact]
+        public void GetCarsInfo_CarsInformation_ReturnDbCars()
+        {
+            //Arrenge
+            var mock = new Mock<IRepository>();
+            // Здесь предполагалось обращение к базе данных но у нас заглушка
+            mock.Setup(repo => repo.GetCars()).Returns(GetCarsColl);
+            var sut = new HomeController(mock.Object);
+
+            ////Act
+            var result = sut.GetCarsInfos();
+
+            //Assert
+            Assert.NotNull(result);
+        }
+
+        private IEnumerable<Car> GetCarsColl()
+        {
+            return new List<Car> {
+            new Car{ Id = 1, Name = "Audi", Year = 2004},
+            new Car{ Id = 1, Name = "Honda", Year = 2020},
+            };
+        }
+
+    }
+}
